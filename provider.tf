037704c679b9c3c3e4c9732b6ec2d969af366620
pwassel <pwassel@gmail.com>
2019-07-30 16:37:55 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,4 @@
 provider "ibm" {
-  ibmcloud_api_key   = "${var.ibmcloud_api_key}"
   softlayer_username = "${var.softlayer_username}"
   softlayer_api_key  = "${var.softlayer_api_key}"
   region             = "${var.region}"
