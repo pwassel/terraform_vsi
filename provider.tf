0ca8383962ddedddc35ff33d2d9819600e407471
pwassel <pwassel@gmail.com>
2019-07-30 16:55:58 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,2 @@
 provider "ibm" {
-  softlayer_username = "${var.softlayer_username}"
-  softlayer_api_key  = "${var.softlayer_api_key}"
-  region             = "${var.region}"
-#  ssh_key            = "${var.ssh_key}"
-  generation         = 1
 }
