d639435f1baef9c38e5a3938ba3e04d635ac24c9
pwassel <pwassel@gmail.com>
2020-04-25 16:16:55 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "ibmcloud_api_key" {}
-variable "softlayer_username" {}
-variable "softlayer_api_key" {}
+# variable "softlayer_username" {}
+# variable "softlayer_api_key" {}
 # variable "ssh_key" {}
 variable "vsi_name" {}
